27c55d02ea46000dde9f1121bb2a2a9a077fb5e2
Kirill Zonov <kzonov@babbel.com>
2018-09-02 18:03:21 +0200

    Added ec2 to the config

--- network.tf
+++ network.tf
@@ -1,3 +1,5 @@
+# Network part
+
 provider "aws" {
   region = "eu-west-1"
   profile = "sandbox"
@@ -40,4 +42,54 @@ resource "aws_internet_gateway" "dod-public" {
   tags {
     Name = "dod-public"
   }
+}
+
+
+# Machine part
+
+# ec2
+# eni
+# elastic_ip
+# sg
+# sg rules
+
+resource "aws_instance" "app" {
+  ami           = "${data.aws_ami.ubuntu.id}"
+  instance_type = "t2.micro"
+
+  # vpc_security_group_ids = ["${aws_security_group.with-http-and-ssh-allowed.id}"]
+
+    network_interface {
+     network_interface_id = "${aws_network_interface.app.id}"
+     device_index = 0
+  }
+
+  tags {
+    Name = "dod-app"
+  }
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-trusty-14.04-amd64-server-*"]
+  }
+
+  owners = ["099720109477"] # Canonical Ltd., the company behind Ubuntu
+}
+
+resource "aws_network_interface" "app" {
+  subnet_id = "${aws_subnet.dod-public.id}"
+  # private_ips = ["172.35.10.100"]
+}
+
+resource "aws_eip" "external-address" {
+  instance = "${aws_instance.app.id}"
+  vpc      = true
+}
+
+output "ip" {
+  value = "${aws_eip.external-address.public_ip}"
 }
