c1821932ffdf6156c3ffb9bf4c839ad2e868494d
Kirill Zonov <kzonov@babbel.com>
2018-09-03 20:11:39 +0200

    Just renaming

--- pre-setup/state_store.tf
+++ pre-setup/state_store.tf
@@ -3,7 +3,7 @@ provider "aws" {
   profile = "sandbox"
 }
 
-resource "aws_s3_bucket" "terraform-state-storage-s3" {
+resource "aws_s3_bucket" "state-storage-s3" {
   bucket = "dod-terraform-remote-state-storage-s3-kzonov"
 
   versioning {
